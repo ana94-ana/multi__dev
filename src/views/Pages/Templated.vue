c4205b5b8b76dc20c2cd442602ab194ca5c1439b
ana94-ana <aniabramishvili94@gmail.com>
2023-06-18 23:57:48 +0400

    add templated div mrkup

--- src/views/Pages/Templated.vue
+++ src/views/Pages/Templated.vue
@@ -39,58 +39,151 @@ import mainLayout from '../../layouts/mainLayout.vue';
  <div>
   <img class="flex items-center mx-auto w-auto h-auto" src="../../assets/images/template-preview.png.png" alt="">
  </div>
+ <div class="bg-[#EEF8FF] space-y-10 py-24">
  <div class="flex flex-col items-center mx-auto space-y-5 ">
       <h1 class="text-5xl text-[#25344F]">Built for  <span class="text-[#6967CE]">Speed and SEO </span></h1>
       <p>Everything you would expect with a website comes standard with us.</p>
     </div>
-    <div class="flex justify-center mx-auto space-x-5 w-3/4 ">
-      <div class="columns-2">
-      <div class="bg-[#FFF8EB]  rounded-lg w-auto ">
-        <div class="m-8 space-y-5">
-        <img  src="../../assets/icone/time-icon.svg.svg" alt="">
-        <p class="text-3xl">Time</p>
-        <p class="text-[#515D72]">Since we have a starting point we’re able to build quickly and efficiently.</p>
+    <div class="columns-2 justify-center mx-auto w-3/4 space-y-5">
+      <div class="container mx-auto h-72 bg-[#FFFFFF] rounded-lg  ">
+        <div class="p-8 space-y-5">
+          <img  src="../../assets/icone/hosting-icon.svg.svg" alt="">
+          <p class="text-3xl">Hosting Included</p>
+        <p class="text-[#515D72]">Hosting fees are built right into the monthly payment.</p>
       </div>
       </div>
-      <div class="bg-[#EBFEFE]  rounded-lg w-auto ">
-        <div class="m-8 space-y-5">
-        <img src="../../assets/icone/money-icon.svg.svg" alt="">
-        <p class="text-3xl">Money</p>
-        <p class="text-[#515D72] ">Our base templates are already built and we just need to reskin it to match your brand.</p>
+      <div class="container mx-auto h-72 bg-[#FFFFFF] rounded-lg  ">
+        <div class="p-8 space-y-5">
+          <img  src="../../assets/icone/fast-icon.svg.svg" alt="">
+          <p class="text-3xl">Wicked Fast</p>
+        <p class="text-[#515D72]">We know how to get your business ranking on Google and all of our websites are built with SEO in mind.</p>
+        </div>
+      </div>
+      <div class="container mx-auto h-72  bg-[#FFFFFF] rounded-lg ">
+        <div class="p-8 space-y-5">
+          <img  src="../../assets/icone/analytics-icon.svg.svg" alt="">
+          <p class="text-3xl">Google Analytics</p>
+        <p class="text-[#515D72]">It’s important to track how your website is being used and Google Analytics helps you understand exactly who your users are and where they’re going.</p>
       </div>
       </div>
-      <div class="bg-[#FFF8EB]  rounded-lg  ">
-        <div class="m-8 space-y-5">
-        <img  src="../../assets/icone/time-icon.svg.svg" alt="">
-        <p class="text-3xl">Time</p>
-        <p class="text-[#515D72]">Since we have a starting point we’re able to build quickly and efficiently.</p>
+      <div class="container mx-auto h-72  bg-[#FFFFFF] rounded-lg  ">
+        <div class="p-8 space-y-5">
+          <img  src="../../assets/icone/edits-icon.svg.svg" alt="">
+          <p class="text-3xl">Unlimited Edits</p>
+        <p class="text-[#515D72]">We’ll help you update any content or photos you might want at any time!</p>
       </div>
       </div>
-      <div class="bg-[#EBFEFE]  rounded-lg  ">
-        <div class="m-8 space-y-5">
-        <img src="../../assets/icone/money-icon.svg.svg" alt="">
-        <p class="text-3xl">Money</p>
-        <p class="text-[#515D72] ">Our base templates are already built and we just need to reskin it to match your brand.</p>
+      <div class="container mx-auto h-72  bg-[#FFFFFF] rounded-lg ">
+        <div class="p-8 space-y-5">
+          <img  src="../../assets/icone/seo-icon.svg.svg" alt="">
+          <p class="text-3xl">SEO Friendly</p>
+        <p class="text-[#515D72]">Since our websites are built from scratch, you can enjoy blazing fast page speed scores which help your users stay on your website but also rank higher with SEO.</p>
       </div>
       </div>
-      <div class="bg-[#FFF8EB]  rounded-lg  ">
-        <div class="m-8 space-y-5">
-        <img  src="../../assets/icone/time-icon.svg.svg" alt="">
-        <p class="text-3xl">Time</p>
-        <p class="text-[#515D72]">Since we have a starting point we’re able to build quickly and efficiently.</p>
+      <div class="container mx-auto h-72  bg-[#FFFFFF] rounded-lg ">
+        <div class="p-8 space-y-5">
+          <img  src="../../assets/icone/profile-icon.svg.svg" alt="">
+          <p class="text-3xl">Google Business Profile</p>
+        <p class="text-[#515D72]">We also help you set up Google Business Profile which is a great tool for local businesses to get more leads.</p>
       </div>
       </div>
-      <div class="bg-[#EBFEFE]  rounded-lg ">
-        <div class="m-8 space-y-5">
-        <img src="../../assets/icone/money-icon.svg.svg" alt="">
-        <p class="text-3xl">Money</p>
-        <p class="text-[#515D72] ">Our base templates are already built and we just need to reskin it to match your brand.</p>
-      </div>
-      </div>
-    </div>
     </div>
   </div>
-    
+</div> 
+<div class="space-y-10 my-24">
+  <div class="flex flex-col items-center mx-auto space-y-5 ">
+      <h1 class="text-5xl text-[#25344F]">Simple and <span class="text-[#6967CE]">Affordable </span>Pricing</h1>
+      <p>We can get your website up and running for as little as $500.</p>
+    </div>
+    <div class="flex flex-row justify-center space-x-8 ">
+    <div class="drop-shadow-2xl bg-[#FFFFFF]  rounded-lg ">
+      <div class="m-8 space-y-5">
+      <p class="text-lg">Basic</p>
+      <p><span class="text-2xl">$199 </span> per month</p>
+      <ul class="space-y-5 list-disc"  >
+        <li class="bg-[#EBF6FF] ">Up to 5 Pages</li>
+        <li>Hosting Included</li>
+        <li>SEO Friendly</li>
+        <li>Lifetime Updates</li>
+        <li>24-month Service Minimum</li>
+      </ul>
+    </div>
+  </div>
+    <div class="drop-shadow-2xl bg-[#25344F] text-[#D4E4F7] rounded-lg ">
+      <div class="m-8 space-y-5">
+      <p class="text-lg">Standard</p>
+      <p><span class="text-2xl">$249 </span> per month</p>
+      <ul class="space-y-5 list-disc">
+        <li class="bg-[#214863]">Up to 10 Pages</li>
+        <li>Hosting Included</li>
+        <li>SEO Friendly</li>
+        <li>Lifetime Updates</li>
+        <li>Google Analytics Setup</li>
+        <li>Google Business Profile Setup</li>
+        <li>24-month Service Minimum</li>
+      </ul>
+    </div>
+  </div>
+    <div class="drop-shadow-2xl bg-[#FFFFFF] rounded-lg  ">
+      <div class="m-8 space-y-5">
+      <p class="text-lg">Premium</p>
+      <p><span class="text-2xl">$299 </span> per month</p>
+      <ul class="space-y-5 list-disc ">
+        <li class="bg-[#FFF7E6] ">Up to 15 Pages</li>
+        <li>Hosting Included</li>
+        <li>SEO Friendly</li>
+        <li>Lifetime Updates</li>
+        <li>Google Analytics Setup</li>
+        <li>Google Business Profile Setup</li>
+        <li>24-month Service Minimum</li>
+      </ul>
+    </div>
+  </div>
+    </div>
+</div>  
+<div class="bg-[#EEF8FF] space-y-10 my-24">
+  <h1 class="text-center text-5xl text-[#25344F]">A Few of Our Template <span class="text-[#6967CE]">Projects</span> </h1>
+  <div class="flex flex-row justify-center mx-auto w-3/4 ">
+  <div class="flex flex-col items-center mx-auto  w-auto mb-20  ">
+    <div>
+      <img src="../../assets/images/templated-uniform.png" alt="">
+    </div>
+    <div class="flex justify-center flex-row w-full rounded-b-lg bg-[#FFFFFF]">
+      <p>ATM Sports</p>
+      <button class="text-[#23A6F0] ml-20 mr-2.5">View Website</button><img src="../../assets/icone/vectorLurji.svg" alt="">
+    </div>
+  </div>
+  <div class="flex flex-col items-center mx-auto  w-auto">
+    <div>
+      <img src="../../assets/images/templated-uniform.png" alt="">
+    </div>
+    <div class="flex justify-center flex-row w-full rounded-b-lg bg-[#FFFFFF]">
+      <p>ATM Sports</p>
+      <button class="text-[#23A6F0]  ml-20 mr-2.5">View Website</button><img src="../../assets/icone/vectorLurji.svg" alt="">
+    </div>
+  </div>
+  <div class="flex flex-col items-center mx-auto w-auto">
+    <div>
+      <img src="../../assets/images/templated-uniform.png" alt="">
+    </div>
+    <div class="flex justify-center flex-row rounded-b-lg w-full bg-[#FFFFFF]">
+      <p>ATM Sports</p>
+      <button class="text-[#23A6F0] ml-20 mr-2.5">View Website</button><img src="../../assets/icone/vectorLurji.svg" alt="">
+    </div>
+  </div>
+</div>
+</div> 
+<div class="flex justify-center mx-auto text-[#25344F] flex-col text-xl space-y-12 divide-y divide-blue-200 w-1/2">
+  <h1 class="text-4xl text-center font-medium">FAQs</h1>
+  <p>What are you using to build the website?</p>
+  <p>How long will it take for my website to be built?</p>
+  <p>Do I have a say in how my website looks?</p>
+  <p>What if I don’t have photos for my website?</p>
+  <p>Can I edit the content on the website myself?</p>
+  <p>Can you transfer blog content from my previous website?</p>
+  <p>Can I cancel anytime?</p>
+  <p>What happens after 24 months?</p>
+</div>
 </div>
  </mainLayout>
 </template>
