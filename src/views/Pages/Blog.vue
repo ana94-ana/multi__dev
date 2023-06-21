60b1f38fa0490ada8bdd3f1b6bb788fc4439c010
ana94-ana <aniabramishvili94@gmail.com>
2023-06-21 23:18:14 +0400

    add blog imige

--- src/views/Pages/Blog.vue
+++ src/views/Pages/Blog.vue
@@ -15,57 +15,51 @@ import mainLayout from '../../layouts/mainLayout.vue';
           <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
         </div>
         <div class="container mx-auto space-y-6">
-          <img src="../../assets//images//a.post-featured-image.png" alt="">
-          <h1 class="text-[#6967CE] text-3xl">Custom WordPress Development: A Comprehensive Guide</h1>
+          <img src="../../assets//images/10-Landing-Page-Dos-and-Donts-1024x534.jpg.png" alt="">
+          <h1 class="text-[#6967CE] text-3xl">10 Landing Page Do’s and Don’ts</h1>
+          <p class=" text-sm text-[#407BEC]">Nick Meagher | August 3, 2022</p>
+          <p class="text-lg text-[#556D9A]">Whether you’re running an advertising campaign or just want to spice up your landing pages, we’ve created a list of 10 Landing Page Do’s and Don’ts to help!</p>
+          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
+        </div>
+        <div class="container mx-auto space-y-6">
+          <img src="../../assets//images/design company.png" alt="">
+          <h1 class="text-[#6967CE] text-3xl">What to Look for When Hiring a Website Design Company</h1>
+          <p class=" text-sm text-[#407BEC]">Nick Meagher | September 8, 2021</p>
+          <p class="text-lg text-[#556D9A]">There are a few key things you need to look for when hiring a website design company in order for you to have a successful website launch.</p>
+          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
+        </div>
+        <div class="container mx-auto space-y-6">
+          <img src="../../assets/images/improve-your-website-copy-1.png.png" alt="">
+          <h1 class="text-[#6967CE] text-3xl">10 Simple Ways to Improve Your Website Copy for Higher Conversions</h1>
+          <p class=" text-sm text-[#407BEC]">Nick Meagher | August 22, 2022</p>
+          <p class="text-lg text-[#556D9A]">In this blog post, we will discuss ten simple ways you can improve your website copy and boost your conversions!</p>
+          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
+        </div>
+        <div class="container mx-auto space-y-6">
+          <img src="../../assets//images/splash.png" alt="">
+          <h1 class="text-[#6967CE] text-3xl">Splash Page vs Landing Page (And When To Use Them)</h1>
+          <p class=" text-sm text-[#407BEC]">Nick Meagher | February 4, 2022</p>
+          <p class="text-lg text-[#556D9A]">What's the difference between a splash page and a landing page? This post will teach you how to choose the right one for your specific needs!</p>
+          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
+        </div>
+        <div class="container mx-auto space-y-6">
+          <img src="../../assets//images/your website.png" alt="">
+          <h1 class="text-[#6967CE] text-3xl">How to Build Your Website in 2021: The Ultimate Guide</h1>
+          <p class=" text-sm text-[#407BEC]">Nick Meagher | February 19, 2021</p>
+          <p class="text-lg text-[#556D9A]">This is the ultimate guide to help you figure out what the best route you should take when your build your website in 2021!</p>
+          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
+        </div>
+        <div class="container mx-auto space-y-6">
+          <img src="../../assets//images/flebsbox.png" alt="">
+          <h1 class="text-[#6967CE] text-3xl">The 3 Colossal Problems Flexbox Fixed Instantly For Me (Flexbox Cheatsheet)</h1>
           <p class=" text-sm text-[#407BEC]">Nick Meagher | May 11, 2023</p>
-          <p class="text-lg text-[#556D9A]">Should you use a WordPress Theme or go with Custom WordPress Development from
-            scratch? It all depends on your business goals.</p>
+          <p class="text-lg text-[#556D9A]">Our comprehensive guide to CSS flexbox layout. This flexbox cheatsheet explains everything you need to know about flexbox.</p>
           <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
         </div>
         <div class="container mx-auto space-y-6">
           <img src="../../assets//images//a.post-featured-image.png" alt="">
           <h1 class="text-[#6967CE] text-3xl">Custom WordPress Development: A Comprehensive Guide</h1>
-          <p class=" text-sm text-[#407BEC]">Nick Meagher | May 11, 2023</p>
-          <p class="text-lg text-[#556D9A]">Should you use a WordPress Theme or go with Custom WordPress Development from
-            scratch? It all depends on your business goals.</p>
-          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
-        </div>
-        <div class="container mx-auto space-y-6">
-          <img src="../../assets//images//a.post-featured-image.png" alt="">
-          <h1 class="text-[#6967CE] text-3xl">Custom WordPress Development: A Comprehensive Guide</h1>
-          <p class=" text-sm text-[#407BEC]">Nick Meagher | May 11, 2023</p>
-          <p class="text-lg text-[#556D9A]">Should you use a WordPress Theme or go with Custom WordPress Development from
-            scratch? It all depends on your business goals.</p>
-          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
-        </div>
-        <div class="container mx-auto space-y-6">
-          <img src="../../assets//images//a.post-featured-image.png" alt="">
-          <h1 class="text-[#6967CE] text-3xl">Custom WordPress Development: A Comprehensive Guide</h1>
-          <p class=" text-sm text-[#407BEC]">Nick Meagher | May 11, 2023</p>
-          <p class="text-lg text-[#556D9A]">Should you use a WordPress Theme or go with Custom WordPress Development from
-            scratch? It all depends on your business goals.</p>
-          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
-        </div>
-        <div class="container mx-auto space-y-6">
-          <img src="../../assets//images//a.post-featured-image.png" alt="">
-          <h1 class="text-[#6967CE] text-3xl">Custom WordPress Development: A Comprehensive Guide</h1>
-          <p class=" text-sm text-[#407BEC]">Nick Meagher | May 11, 2023</p>
-          <p class="text-lg text-[#556D9A]">Should you use a WordPress Theme or go with Custom WordPress Development from
-            scratch? It all depends on your business goals.</p>
-          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
-        </div>
-        <div class="container mx-auto space-y-6">
-          <img src="../../assets//images//a.post-featured-image.png" alt="">
-          <h1 class="text-[#6967CE] text-3xl">Custom WordPress Development: A Comprehensive Guide</h1>
-          <p class=" text-sm text-[#407BEC]">Nick Meagher | May 11, 2023</p>
-          <p class="text-lg text-[#556D9A]">Should you use a WordPress Theme or go with Custom WordPress Development from
-            scratch? It all depends on your business goals.</p>
-          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
-        </div>
-        <div class="container mx-auto space-y-6">
-          <img src="../../assets//images//a.post-featured-image.png" alt="">
-          <h1 class="text-[#6967CE] text-3xl">Custom WordPress Development: A Comprehensive Guide</h1>
-          <p class=" text-sm text-[#407BEC]">Nick Meagher | May 11, 2023</p>
+          <p class=" text-sm text-[#407BEC]">Grant Meagher | April 26, 2018</p>
           <p class="text-lg text-[#556D9A]">Should you use a WordPress Theme or go with Custom WordPress Development from
           scratch? It all depends on your business goals.</p>
         <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
