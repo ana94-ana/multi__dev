d1fef685f3237439e6690438e99e8df0d50942b1
ana94-ana <aniabramishvili94@gmail.com>
2023-06-22 21:52:18 +0400

    add blog markup div

--- src/views/Pages/Blog.vue
+++ src/views/Pages/Blog.vue
@@ -4,7 +4,10 @@ import mainLayout from '../../layouts/mainLayout.vue';
 
 <template>
   <mainLayout>
-    <div class="">
+    <div class="space-y-10">
+      <div class="text-[#FFFFFF] bg-[#6967CE]   ">
+        <h1 class="py-20 text-center text-5xl">Blog</h1>
+      </div>
       <div class="columns-2 justify-center mx-auto w-3/4 space-y-5">
         <div class="container mx-auto space-y-6">
           <img src="../../assets//images//a.post-featured-image.png" alt="">
@@ -15,21 +18,28 @@ import mainLayout from '../../layouts/mainLayout.vue';
           <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
         </div>
         <div class="container mx-auto space-y-6">
-          <img src="../../assets//images/10-Landing-Page-Dos-and-Donts-1024x534.jpg.png" alt="">
+          <img src="../../assets//images/0Landingdiv.row.png" alt="">
           <h1 class="text-[#6967CE] text-3xl">10 Landing Page Do’s and Don’ts</h1>
           <p class=" text-sm text-[#407BEC]">Nick Meagher | August 3, 2022</p>
           <p class="text-lg text-[#556D9A]">Whether you’re running an advertising campaign or just want to spice up your landing pages, we’ve created a list of 10 Landing Page Do’s and Don’ts to help!</p>
           <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
         </div>
         <div class="container mx-auto space-y-6">
-          <img src="../../assets//images/design company.png" alt="">
+          <img src="../../assets/images/design company.png" alt="">
           <h1 class="text-[#6967CE] text-3xl">What to Look for When Hiring a Website Design Company</h1>
           <p class=" text-sm text-[#407BEC]">Nick Meagher | September 8, 2021</p>
           <p class="text-lg text-[#556D9A]">There are a few key things you need to look for when hiring a website design company in order for you to have a successful website launch.</p>
           <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
         </div>
         <div class="container mx-auto space-y-6">
-          <img src="../../assets/images/improve-your-website-copy-1.png.png" alt="">
+          <img src="../../assets//images/create.png" alt="">
+          <h1 class="text-[#6967CE] text-3xl">How to Create a jQuery Smooth Scrolling Effect</h1>
+          <p class=" text-sm text-[#407BEC]">Nick Meagher | July 17, 2018</p>
+          <p class="text-lg text-[#556D9A]">Creating a one-page jQuery smooth scrolling navigation where items link to a section on the page is a very common request and relatively simple to do with jQuery!</p>
+        <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
+      </div> 
+        <div class="container mx-auto space-y-6">
+          <img src="../../assets/images/way.png" alt="">
           <h1 class="text-[#6967CE] text-3xl">10 Simple Ways to Improve Your Website Copy for Higher Conversions</h1>
           <p class=" text-sm text-[#407BEC]">Nick Meagher | August 22, 2022</p>
           <p class="text-lg text-[#556D9A]">In this blog post, we will discuss ten simple ways you can improve your website copy and boost your conversions!</p>
@@ -41,7 +51,7 @@ import mainLayout from '../../layouts/mainLayout.vue';
           <p class=" text-sm text-[#407BEC]">Nick Meagher | February 4, 2022</p>
           <p class="text-lg text-[#556D9A]">What's the difference between a splash page and a landing page? This post will teach you how to choose the right one for your specific needs!</p>
           <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
-        </div>
+        </div> 
         <div class="container mx-auto space-y-6">
           <img src="../../assets//images/your website.png" alt="">
           <h1 class="text-[#6967CE] text-3xl">How to Build Your Website in 2021: The Ultimate Guide</h1>
@@ -57,13 +67,16 @@ import mainLayout from '../../layouts/mainLayout.vue';
           <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
         </div>
         <div class="container mx-auto space-y-6">
-          <img src="../../assets//images//a.post-featured-image.png" alt="">
-          <h1 class="text-[#6967CE] text-3xl">Custom WordPress Development: A Comprehensive Guide</h1>
-          <p class=" text-sm text-[#407BEC]">Grant Meagher | April 26, 2018</p>
-          <p class="text-lg text-[#556D9A]">Should you use a WordPress Theme or go with Custom WordPress Development from
-          scratch? It all depends on your business goals.</p>
-        <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
-      </div>
+          <img src="../../assets//images/flebsbox.png" alt="">
+          <h1 class="text-[#6967CE] text-3xl">The 3 Colossal Problems Flexbox Fixed Instantly For Me (Flexbox Cheatsheet)</h1>
+          <p class=" text-sm text-[#407BEC]">Nick Meagher | May 11, 2023</p>
+          <p class="text-lg text-[#556D9A]">Our comprehensive guide to CSS flexbox layout. This flexbox cheatsheet explains everything you need to know about flexbox.</p>
+          <button class="bg-[#FFB902] rounded-full px-8 py-4">Read Post</button>
+        </div> 
     </div>
+    <div class="flex flex-col text-center h-full bg-[#6967CE] text-6xl text-[#FFFFFF] pt-20 ">
+        <p>Ready to work with us? </p>
+        <p class="text-[#FFB902] pb-20 underline underline-offset-8 "> Let's build something awesome.</p>
+      </div>
   </div>
 </mainLayout></template>
