ccb964eb678d5434107300c47fe563d1a7370d14
ana94-ana <aniabramishvili94@gmail.com>
2023-06-24 23:03:08 +0400

    contact and custom response

--- src/views/Pages/Custom.vue
+++ src/views/Pages/Custom.vue
@@ -6,24 +6,23 @@ import mainLayout from '../../layouts/mainLayout.vue';
   <mainLayout>
     <div>
       <div class="flex flex-row-reverse bg-[#6967CE]">
-        <div class="flex justify-end mx-auto w-1/3 h-auto ">
-          <img class="w-full" src="../../assets/images/custom-websites.png.png" alt="">
+        <div class="flex justify-end mx-auto  h-auto md:w-1/3">
+          <img class="w-0 md:w-full" src="../../assets/images/custom-websites.png.png" alt="">
         </div>
-        <div class="flex flex-col items-center mx-auto  ">
-          <div class="text-left mx-52 space-y-5 my-20">
-          <h1
-            class="text-6xl text-left text-[#FFB902]  ">
+        <div class="flex flex-col items-center  mx-auto  ">
+          <div class="text-center space-y-5 my-20 md:mx-52 md:text-left">
+          <h1 class="text-center text-6xl  text-[#FFB902]  md:text-left">
             Cutom <span class="text-[#FFFFFF]">Websites</span></h1>
           <p class=" text-[#BDC8DB] text-xl">Whether you’re looking to build a custom marketing website or the next
             Twitter we can help you build anything
             you’re envisioning.</p>
-          <button class="flex w-44 text-[#FFFFFF] border rounded-full py-4 px-8 text-sm"> Get In
+          <button class="flex  w-44 text-[#FFFFFF] border rounded-full py-4 px-8 text-sm"> Get In
             Touch</button>
         </div>
       </div>
       </div>
       <div>
-        <div class="flex flex-row-reverse">
+        <div class="flex flex-col-reverse md:flex-row-reverse">
           <div>
             <img src="../../assets/images/custom.image.leptop.png" alt="">
           </div>
