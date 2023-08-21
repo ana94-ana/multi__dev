c8dd3405c7a37a748893441b4528691b07002216
ana94-ana <aniabramishvili94@gmail.com>
2023-08-21 17:42:33 +0400

    remove ourwork vue

--- src/components/ourWork/OurWorkcards.vue
+++
@@ -1,7 +0,0 @@
-<script setup>
-import CloudvaraCard from './CloudvaraCard.vue';
-</script>
-<template>
-
-<CloudvaraCard class="flex justify-center mr-3 flex-col rounded-r-lg  bg-gradient-to-b from-[#F3C57A] via-[#F39369] to-[#E94867]  md:rounded-lg md:flex-row md:mx-auto md:w-3/4 "/>
-</template>
