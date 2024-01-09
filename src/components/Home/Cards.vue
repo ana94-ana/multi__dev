f529f846e1c9976a7cebd1ee39d4a2578a2b0178
ana94-ana <aniabramishvili94@gmail.com>
2024-01-09 21:51:53 +0400

    cards add

--- src/components/Home/Cards.vue
+++ src/components/Home/Cards.vue
@@ -2,16 +2,71 @@
 
 </script>
 <template>
-    <div>
-        <h1>გალერეა</h1>
-        <div>
+<h1 class="text-center font-semibold text-6xl">გალერეა</h1>
+    <div class="flex flex-row space-x-10 mx-10"> 
+        <div class="flex flex-col w-auto ">
             <div>
-                <img src="../../assets/galery/image/monaliza.png" alt="">
+                <img src="@/assets/galery/image/monaliza.png" alt="">
             </div>
-            <h3>5000$</h3>
-            <button class="bg-[#1C5178]">ყიდვა</button>
-            <img src="../../assets/icone/love.svg" alt="">
-            <img src="../../assets/icone" alt="">
+            <div class="flex flex-row justify-between ">
+                <div class="space-y-3 my-3 text-xl">
+                    <h3>მონა ლიზა</h3>
+                    <h3>5000$</h3>
+                    <button class="bg-[#1C5178] w-32 h-11">ყიდვა</button>
+                </div>
+                <div class="space-y-3 my-3">
+                    <img src="@/assets/icone/love.svg" alt="">
+                    <img src="../../assets//icone/love.svg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-auto ">
+            <div>
+                <img src="../../assets/galery/image/qalbatoniErminit.png" alt="">
+            </div>
+            <div class="flex flex-row justify-between ">
+                <div class="space-y-3 my-3 text-xl">
+                    <h3>ქალბატონი ერმინით</h3>
+                    <h3>5000$</h3>
+                    <button class="bg-[#1C5178] w-32 h-11">ყიდვა</button>
+                </div>
+                <div class="space-y-3 my-3">
+                    <img src="../../assets/icone/love.svg" alt="">
+                    <img src="../../assets/icone/love.svg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-auto ">
+            <div>
+                <img src="../../assets/galery/image/salvator.png" alt="">
+            </div>
+            <div class="flex flex-row justify-between ">
+                <div class="space-y-3 my-3 text-xl">
+                    <h3>სალვატორ მუნდი</h3>
+                    <h3>5000$</h3>
+                    <button class="bg-[#1C5178] w-32 h-11">ყიდვა</button>
+                </div>
+                <div class="space-y-3 my-3">
+                    <img src="../../assets/icone/love.svg" alt="">
+                    <img src="../../assets/icone/love.svg" alt="">
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col w-auto ">
+            <div>
+                <img src="../../assets/galery/image/mixakisMadona.png" alt="">
+            </div>
+            <div class="flex flex-row justify-between ">
+                <div class="space-y-3 my-3 text-xl">
+                    <h3>მიხაკის მადონა</h3>
+                    <h3>5000$</h3>
+                    <button class="bg-[#1C5178] w-32 h-11">ყიდვა</button>
+                </div>
+                <div class="space-y-3 my-3">
+                    <img src="../../assets/icone/love.svg" alt="">
+                    <img src="../../assets/icone/love.svg" alt="">
+                </div>
+            </div>
         </div>
     </div>
 </template>
